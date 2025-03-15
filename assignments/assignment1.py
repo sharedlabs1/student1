7e52bfbca8a405602ebb9a92a994fc51320cbd16
learnlyticaassessments <assessments@learnlytica.com>
2025-03-15 23:09:02 +0530

    Update assignment1.py

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -1,1 +1,72 @@
+"""
+CS101 Assignment 1: Data Analysis Functions
 
+Complete the following functions according to their docstrings.
+Do not modify the function signatures.
+"""
+
+def calculate_mean(numbers):
+    """
+    Calculate the arithmetic mean (average) of a list of numbers.
+    
+    Args:
+        numbers (list): A list of numbers (int or float)
+        
+    Returns:
+        float: The arithmetic mean of the numbers
+        
+    Raises:
+        ValueError: If the list is empty
+    """
+    # TODO: Implement this function
+   
+
+
+def find_median(numbers):
+    """
+    Find the median value of a list of numbers.
+    If the list has an odd number of elements, return the middle element.
+    If the list has an even number of elements, return the average of the two middle elements.
+    
+    Args:
+        numbers (list): A list of numbers (int or float)
+        
+    Returns:
+        float: The median value
+        
+    Raises:
+        ValueError: If the list is empty
+    """
+    # TODO: Implement this function
+
+
+
+def count_frequency(items):
+    """
+    Count the frequency of each item in the list.
+    
+    Args:
+        items (list): A list of items
+        
+    Returns:
+        dict: A dictionary mapping each item to its frequency
+    """
+    # TODO: Implement this function
+   
+
+
+def find_range(numbers):
+    """
+    Calculate the range of a list of numbers (max - min).
+    
+    Args:
+        numbers (list): A list of numbers (int or float)
+        
+    Returns:
+        float: The range of the numbers
+        
+    Raises:
+        ValueError: If the list is empty
+    """
+    # TODO: Implement this function
+   
